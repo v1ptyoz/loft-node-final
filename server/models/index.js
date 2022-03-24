cfd658a8999aa0cb05da4a178cd5fa21460c75a7
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2022-03-24 19:18:36 +1000

    methods for messages

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,13 @@
 const User = require('./schemas/users');
 const News = require('./schemas/news');
+const Messages = require('./schemas/messages');
 
 module.exports.getUserByName = async (username) => {
-    return User.findOne({username})
+    return User.findOne({ username })
 }
 
 module.exports.getUserById = async (id) => {
-    return User.findById({_id: id});
+    return User.findById({ _id: id });
 }
 
 module.exports.getUsers = async () => {
@@ -14,7 +15,7 @@ module.exports.getUsers = async () => {
 }
 
 module.exports.updateUserPermissions = async (id, permission) => {
-    await User.findByIdAndUpdate(id, {permission: permission})
+    await User.findByIdAndUpdate(id, { permission: permission })
 }
 
 module.exports.deleteUser = async (id) => {
@@ -22,7 +23,7 @@ module.exports.deleteUser = async (id) => {
 }
 
 module.exports.updateUser = async function (user, data) {
-    const {surName, firstName, middleName, avatar, newPassword} = data;
+    const { surName, firstName, middleName, avatar, newPassword } = data;
     if (surName) user.surName = surName;
     if (firstName) user.firstName = firstName;
     if (middleName) user.middleName = middleName;
@@ -35,7 +36,7 @@ module.exports.updateUser = async function (user, data) {
 }
 
 module.exports.createUser = async (data) => {
-    const {username, surName, firstName, middleName, password} = data;
+    const { username, surName, firstName, middleName, password } = data;
     const newUser = new User({
         username,
         surName,
@@ -43,9 +44,9 @@ module.exports.createUser = async (data) => {
         middleName,
         image: '',
         permission: {
-            chat: {C: true, R: true, D: true, U: true},
-            news: {C: true, R: true, D: true, U: true},
-            settings: {C: true, R: true, D: true, U: true},
+            chat: { C: true, R: true, D: true, U: true },
+            news: { C: true, R: true, D: true, U: true },
+            settings: { C: true, R: true, D: true, U: true },
         }
     })
     newUser.setPassword(password);
@@ -57,7 +58,7 @@ module.exports.getNews = async () => {
     return await News.find();
 }
 
-module.exports.createNews = async ({text, title, user}) => {
+module.exports.createNews = async ({ text, title, user }) => {
     const news = new News({
         text,
         title,
@@ -72,5 +73,40 @@ module.exports.deleteNews = async (id) => {
 }
 
 module.exports.updateNews = async (id, title, text) => {
-    await News.findByIdAndUpdate(id, {title: title, text: text})
+    await News.findByIdAndUpdate(id, { title: title, text: text })
+}
+
+module.exports.getMessagesBySenderIdAndRecipientId = async (senderId, recipientId) => {
+    const record = await Messages.findOne({ "sender": senderId, "recipient": recipientId })
+    return record;
+}
+
+module.exports.createSenderAndPutMessage = async (senderId, recipientId, roomId, text) => {
+    const sender = new Messages({
+        sender: senderId,
+        recipient: recipientId,
+        data: {
+            senderId,
+            recipientId,
+            roomId,
+            messages: []
+        }
+    })
+    sender.data.messages.push(text);
+    await sender.save();
+    return sender;
+}
+
+module.exports.addMessageForSender = async (senderId, recipientId, roomId, text) => {
+    const record = await Messages.findOne({ "sender": senderId, "recipient": recipientId, "data.roomId": roomId });
+    record.data.messages.push(text);
+    await record.save();
+    return record;
+}
+
+module.exports.dropAllMessages = async () => {
+    const messages = await Messages.find();
+    messages.forEach(async message => {
+        await Messages.findByIdAndDelete(message._id)
+    })
 }
